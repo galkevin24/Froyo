b1ae35f6452d5fa8a33921d3c059cb78d075b601
Jose Galeana <kevingaleana@yahoo.com>
2025-02-03 12:45:50 -0600

    all done

---
+++ froyo.js
@@ -0,0 +1,20 @@
+const flavorcount= ["vanilla","vanilla","vanilla","strawberry", "coffee", "coffee"];
+console.log(flavorcount)
+
+const flavorsArray = flavorcount.split('.');
+
+const flavorcount = {};
+
+flavorsArray.ForEach ( flavor =>)
+
+function countFlavors(flavors) {
+if (flavorCount[flavor]) {
+    flavorcount[flavor]++;
+} else {
+    flavorcount[flavor] = 1;
+}
+});
+
+return flavorCount;
+
+console.log(flavorcount);
